ca834d044a7bcfe01e66d2d2113ab36af6a0f44f
iambalakrishnan <imbkrishnaa@gmail.com>
2022-09-29 15:36:17 +0530

    configuration completed

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-from ensurepip import version
 import setuptools
+
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 __version__ = "0.0.0"
 
 REPO_NAME = "IPYNBrenderer"
-AUTHOR_NAME = "iambalakrishnan"
-AUTHON_EMAIL = "imbkrishnaa@gmail.com"
+AUTHOR_USER_NAME = "iambalakrishnan"
 SRC_REPO = "IPYNBrenderer"
+AUTHOR_EMAIL = "imbkrishnaa@gmail.com"
 
-setuptools(
+setuptools.setup(
     name=SRC_REPO,
     version=__version__,
+    author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package",
-    Long_description=long_description,
-    long_description_contect="text/markdown",
-    url=f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}"
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "bug Tracker": f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
-
 )
